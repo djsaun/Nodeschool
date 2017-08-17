d4759cbd65d96925b99cc6df5d45da6b5900f58e
djsaun <djsaun@gmail.com>
2017-08-17 16:48:10 -0500

    Completed My First I/O challenge

---
+++ learnyounode/first-io.js
@@ -0,0 +1,8 @@
+var fs = require('fs')
+
+const filename = process.argv[2];
+const fileData = fs.readFileSync(filename);
+const fileStr = fileData.toString();
+const numNewLines = fileStr.split('\n').length-1;
+
+console.log(numNewLines);
